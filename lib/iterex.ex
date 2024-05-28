0746e7f813972cab4ceb86d996559a67b01f094a
James Harton <james@harton.nz>
2024-05-29 10:25:51 +1200

    chore: empty mix project.

---
+++ lib/iterex.ex
@@ -0,0 +1,18 @@
+defmodule Iterex do
+  @moduledoc """
+  Documentation for `Iterex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Iterex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/iterex_test.exs
@@ -0,0 +1,8 @@
+defmodule IterexTest do
+  use ExUnit.Case
+  doctest Iterex
+
+  test "greets the world" do
+    assert Iterex.hello() == :world
+  end
+end
