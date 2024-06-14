45201eb5b0f56c2f46e486ddf1c974d2df7a7c76
James Harton <james@harton.nz>
2024-06-14 14:38:16 +1200

    improvement(IO.Stream): Add `IntoIterable` for `IO.Stream`.

--- lib/iter/into_iterable.ex
+++ lib/iter/into_iterable.ex
@@ -4,8 +4,8 @@ defprotocol Iter.IntoIterable do
 
   Required by `Iter.from/1` and others.
 
-  By default this protocol is implemented for `List`, `Map`, `MapSet` and
-  `Range` and `Date.Range` as well as all iterex's internal types.
+  By default this protocol is implemented for `List`, `Map`, `MapSet`, `Range`,
+  `Date.Range` and `IO.Stream` as well as all iterex's internal types.
 
   Allows the data and the iterable for that data to be separate.  It could be as
   simple as a reference to some external data source and a position marker or it
---
+++ test/iter/into_iterable/io_stream_test.exs
@@ -0,0 +1,31 @@
+defmodule Iter.IntoIterable.IO.StreamTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+
+  test "it can iterate an IO stream" do
+    words = words()
+    {:ok, pid} = StringIO.open(words)
+
+    expected = String.split(words, ~r/(?<=\n)/)
+
+    actual =
+      pid
+      |> IO.stream(:line)
+      |> Iter.IntoIterable.into_iterable()
+      |> Iter.Iterable.to_list()
+
+    assert actual == expected
+  end
+
+  defp words do
+    how_many = 20 + :rand.uniform(20)
+
+    words =
+      1..how_many
+      |> Iter.from()
+      |> Iter.map(fn _ -> Faker.Lorem.word() end)
+      |> Enum.join("\n")
+
+    words
+  end
+end
